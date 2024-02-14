f19e75ae85907e5ca45dfbf8bb108b0d030f707d
Alexander Chairs <alexander_chairs@student.uml.edu>
2024-02-13 21:54:46 -0500

    shsh

--- project/main.c
+++ project/main.c
@@ -29,114 +29,81 @@
 
 
 int main(int argc, const char * argv[]) {
-
     pid_t parent_pid = getpid();
 
     printf("parent pid is %d\n", parent_pid);
-	pid_t child_pid;
-    /* = fork();
-	if (child_pid< 0) {// Error occurred
-		printf("Fork ID=%d failed/n", child_pid);
-		fprintf(stderr, "Fork failed");
-		return 1;
-	} else if (child_pid==0) {
-		child_pid = getpid();
-		//execlp("/bin/ls", "ls", NULL);
-	} 
-	printf("Started child with pid %d\n", child_pid); 
-	wait(NULL);
-	printf("Child (PID %d) finished\n", child_pid);*/
+
 	//Does argv give the address or actual value of 2nd input?
     //Make an array and stoire the PID vales in it
+
 	int counter = atoi(argv[1]);
-	char *file = argv[2];
-	int myArray[counter];
+	int finished_pids[counter];	
 	int status;
-    child_pid = 0;
+
     //Trying ot figureout the loop based off the number input by the user in terminal to identify how many children to make
     //wait() - returns PID when finished
     //gets each program in the folder and forks
 	const char *files[5] = {"test1.c", "test2.c", "test3.c", "test4.c", "test5.c"};
-	if (file==NULL) {
-		//const char *arg = argv[2];
-		for(int i = 0; i < counter-1; i++)            
+    for(int i = 0; i < counter; i++)            
+    {
+		//int random = rand() % (counter+1); //https://www.geeksforgeeks.org/c-rand-function/
+        pid_t child_pid = fork();
+
+		if(child_pid < 0)
 		{
-			//int random = rand() % (counter); //https://www.geeksforgeeks.org/c-rand-function/
-			//pid_t child_pid = fork();
-			myArray[i+1] = fork();
-			wait(&status);
-			child_pid = myArray[i+1];
-			//int pid = execlp(arg[random], "ls", NULL);
-			if(myArray[i+1] == 0)
-			{
-				myArray[i+1] = getpid();
-				//child_pid = myArray[i+1];
-			//STart Child precces
-			//RUN THE Excecutable which will run a specific test file
-			//Either make a random number generator to chppse which executable to run to make it run 
-			//Like a round robin, 1, 2, 3, 4, 5, 1, 2, ....
-				//execlp(files[random], "ls", NULL); //From notes 
-				
-			} else if (myArray[i+1] < 0) {
-				while (myArray[i+1] < 0) {
-					printf("fork failed, again\n");
-					myArray[i+1] = fork();
-					//printf("started child %d with pid %d\n", i+1, myArray[i+1]);
-					//pid = execlp(files[random], NULL);
-				}
-			}
-			//printf("started child %d with pid %d\n", i+1, myArray[i+1]);
-			//wait(NULL);
-			//printf("Child (PID %d) finished\n", myArray[i+1]);
+			//error
+			perror("fork");
 		}
-	}	else	{
-		for(int i = 0; i < counter; i++)            
+		else if(child_pid == 0)
+        {
+			//finished_pids[i] = getpid();
+			printf("started child %d with pid %d\n", i+1, get_pid());
+			//STart Child precces
+        //RUN THE Excecutable which will run a specific test file
+        //Either make a random number generator to chppse which executable to run to make it run 
+        //Like a round robin, 1, 2, 3, 4, 5, 1, 2, ....
+
+            //execlp(files[random], "ls", NULL); //From notes 
+        } 
+
+        /*else if (child_pid < 0) {
+			while (child_pid < 0) {
+				printf("fork failed, again\n");
+				child_pid = fork();
+				execlp(files[random], "ls", NULL);
+			}
+		}
+        */
+
+    }
+
+	for(int i = 0; i < counter; i++)
+	{
+		int status;
+		pid_t finished_pid; 
+		finished_pid = wait(&status);
+
+		if(finished_pid > 0)
 		{
-			//int random = rand() % (counter); //https://www.geeksforgeeks.org/c-rand-function/
-			//pid_t child_pid = fork();
-			myArray[i+1] = execlp(file, NULL);
-			//int pid = execlp(arg[random], "ls", NULL);
-			if(myArray[i+1] == 0)
-			{
-				myArray[i+1] = getpid();
-				//child_pid = myArray[i+1];
-			//STart Child precces
-			//RUN THE Excecutable which will run a specific test file
-			//Either make a random number generator to chppse which executable to run to make it run 
-			//Like a round robin, 1, 2, 3, 4, 5, 1, 2, ....
-				//execlp(files[random], "ls", NULL); //From notes 
-				
-			} else if (myArray[i+1] < 0) {
-				while (myArray[i+1] < 0) {
-					printf("fork failed, again\n");
-					myArray[i+1] = execlp(file, NULL);
-				}
-			}
-			printf("Running program %s in process %d\n", file, myArray[i+1]);
-			wait(NULL);
-			printf("Child (PID %d) finished\n", myArray[i+1]);
+			//Store pid of finished child into an array
+			finished_pids[i] = finished_pid;
 		}
-	}	
-	if (file==NULL) {
-		printf("Started child with pid %d\n", child_pid); 
-		wait(NULL);
-		printf("Child (PID %d) finished\n", child_pid);
+
 	}
+
     //For loop to see when the PIDs have finished using wait()
     //Order in whihc they finish has nothig to do with how they started
     //Loop that assigns the CHild number to the order of which the PIDs have finished
-    /*for(int i = 1; i <= counter; i++)
+    for(int i = 0; i <= counter; i++)
     {
         int num = wait(NULL); //From notes and OS_lec3_IPC.pdf		
-        int* location = myArray[i];
-
-        if(location == myArray[i])
+        int* location = finished_pids;
+        if(location <= finished_pids + counter)
         {
-            printf("Child %d (PID %n) Finished\n",num,location);
+            printf("Child %d (PID %n) Finished\n", i + 1, num);
         }
-
-    }*/
-
+    }
+    
 
     /*
     if(child_pid == 0)
@@ -144,7 +111,6 @@ int main(int argc, const char * argv[]) {
         cout << "started child with pid " << getpid() << endl;
         
         //Child proccess logic
-
         cout << "Child (PID " << getpid() << ") Finished" << endl;
     }
     */
